018ef4d29fd5029255581ad42bae2e0cbfd65f6a
Gustavo Pariente <gustavopariente@192.168.1.73>
2026-07-01 09:58:39 -0500

    fix: importar useEffect en Trabajadores

--- src/pages/Trabajadores.jsx
+++ src/pages/Trabajadores.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef } from 'react'
+import { useState, useMemo, useRef, useEffect } from 'react'
 import { useApp } from '../context/AppContext'
 import {
   formatMoney, formatDate, calcRealSalary, calcDailySalary, calcProratedSalary,
